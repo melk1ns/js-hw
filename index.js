0e3d3eaf0ea6784b852aba6648a231f89177fbd9
melk1ns <ponomarlera1@gmail.com>
2024-02-12 21:47:40 +0200

    hw 1

---
+++ index.js
@@ -0,0 +1,21 @@
+let age = 15;
+console.log(age);
+
+let name = "Valeria";
+console.log(name);
+
+let isStudent = false;
+console.log(isStudent);
+
+let myString =
+  "Every evening I died and every evening I was born again. Resurrected.";
+console.log(myString);
+
+let myNumber = 0;
+console.log(myNumber + 10);
+
+let myNull = null;
+console.log(myNull);
+
+const userName = prompt("Please, enter your name");
+alert("Welcome " + userName);
