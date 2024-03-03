a63f3abc71f341121a2549e732d7bc0130b0a6dd
melk1ns <ponomarlera1@gmail.com>
2024-03-04 00:28:50 +0200

    hw 2

---
+++ index.js
@@ -0,0 +1,30 @@
+//task 1
+const degreesC = 200;
+console.log("degreesF", degreesC + 32);
+//task 2
+const days = 4;
+console.log("hours", days * 24);
+console.log("minutes", days * 24 * 60);
+//task 3
+console.log("hp", Math.random() * (200 - 35) + 35) - 35;
+console.log("energy", Math.random() * (350 - 47) + 47) - 47;
+//task 4
+const sum = 3050;
+console.log(sum - (sum * 10) / 100);
+//task 5
+const number = 2.23456789009876543;
+console.log(Math.floor(number));
+//task 6
+const number1 = "34.235";
+console.log(parseFloat(number1));
+//task 7
+const number2 = "1345345bfgtyru";
+console.log(parseInt(number2));
+//task 8
+const number3 = 289;
+console.log(Math.sqrt(number3));
+//task 9
+const num = 15;
+const str = "15";
+console.log(parseInt(str));
+console.log(toString(num));
