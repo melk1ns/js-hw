af48362aee08900219f2f5daf2a915858c325d82
melk1ns <ponomarlera1@gmail.com>
2024-07-01 16:39:59 +0300

    .

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-// // task 1
-// const showMessage = () => alert("Hello World!");
+// task 1
+const showMessage = () => alert("Hello World!");
 
-// showMessage();
-// // task 2
-// const randomNumber = Math.floor(Math.random() * 100) + 1;
+showMessage();
+// task 2
+const randomNumber = Math.floor(Math.random() * 100) + 1;
 
-// const check = (randomNumber, guessNumber) => {
-//   if (guessNumber === randomNumber) {
-//     return "Число співпадає";
-//   } else {
-//     return "Число не співпадає.";
-//   }
-// };
-// // task 4
-// const applyCallback = (array, callback) => {
-//   const resultArray = [];
-//   for (let i = 0; i < array.length; i++) {
-//     resultArray.push(callback(array[i]));
-//   }
-//   return resultArray;
-// };
+const check = (randomNumber, guessNumber) => {
+  if (guessNumber === randomNumber) {
+    return "Число співпадає";
+  } else {
+    return "Число не співпадає.";
+  }
+};
+// task 4
+const applyCallback = (array, callback) => {
+  const resultArray = [];
+  for (let i = 0; i < array.length; i++) {
+    resultArray.push(callback(array[i]));
+  }
+  return resultArray;
+};
 
-// const numbers = [5, 2, 3, 1, 5];
-// const callbackFunction = (num) => num - 1;
+const numbers = [5, 2, 3, 1, 5];
+const callbackFunction = (num) => num - 1;
 
-// const result = applyCallback(numbers, callbackFunction);
-// console.log(result);
+const result = applyCallback(numbers, callbackFunction);
+console.log(result);
 //task 5
 const calculateDiscountPrice = (price, discount, callback) => {
   return callback(price, discount);
